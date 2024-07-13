84b023d0d2a4499f0fcb6bc1fe9db83aa14bc210
Eduardo Dantas <eduardodanttas@icloud.com>
2024-07-13 13:47:35 -0300

    create project

---
+++ backend/src/test/java/com/eduardo/projectpro/ProjectProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.projectpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
